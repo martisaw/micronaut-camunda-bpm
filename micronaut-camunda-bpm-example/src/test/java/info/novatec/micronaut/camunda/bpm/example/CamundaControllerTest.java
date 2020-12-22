ef27991447b90fc27f8b7b9bdf8eedc80d31c0bc
Martin Sawilla <martin.sawilla@gmail.com>
2020-12-22 14:12:53 +0100

    map CamundaController to "/example"

--- micronaut-camunda-bpm-example/src/test/java/info/novatec/micronaut/camunda/bpm/example/CamundaControllerTest.java
+++ micronaut-camunda-bpm-example/src/test/java/info/novatec/micronaut/camunda/bpm/example/CamundaControllerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CamundaControllerTest {
 
     @Inject
-    @Client("/default")
+    @Client("/example")
     RxHttpClient client;
 
     @Test
