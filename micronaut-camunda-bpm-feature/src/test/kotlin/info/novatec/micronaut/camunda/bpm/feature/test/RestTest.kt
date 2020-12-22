c27117706c237f2863bab1124c4d7183c8d7365c
Martin Sawilla <martin.sawilla@gmail.com>
2020-12-22 11:16:41 +0100

    Tests only get executed if the bean Server::class (the Jetty Server) exists.

--- micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/RestTest.kt
+++ micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/RestTest.kt
@@ -1,22 +1,28 @@
 package info.novatec.micronaut.camunda.bpm.feature.test
 
+import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.eclipse.jetty.server.Server
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
+
 /**
- * Tests if the REST API is accessible
+ * Simple Test to check if the REST API runs.
  *
  * @author Martin Sawilla
  */
+@Requires(beans = [Server::class])
 @MicronautTest
 class RestTest {
+
     @Inject
     @field:Client("/engine-rest")
     var client: RxHttpClient? = null
+
     @Test
     fun engine() {
         val request: HttpRequest<String> = HttpRequest.GET("/engine")
--- micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/WebappTest.kt
+++ micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/WebappTest.kt
@@ -1,14 +1,22 @@
 package info.novatec.micronaut.camunda.bpm.feature.test
 
+import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.eclipse.jetty.server.Server
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
+/**
+ * Simple Test to check if the Webapps run.
+ *
+ * @author Martin Sawilla
+ */
+@Requires(beans = [Server::class])
 @MicronautTest
 class WebappTest {
     @Inject
