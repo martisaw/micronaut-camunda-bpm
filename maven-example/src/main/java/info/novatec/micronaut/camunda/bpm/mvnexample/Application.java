39eec66c0f3d76aa8f5454821296e08ea39d0057
Martin Sawilla <martin.sawilla@gmail.com>
2021-01-22 10:18:15 +0100

    clean mn launch

---
+++ maven-example/src/main/java/info/novatec/micronaut/camunda/bpm/mvnexample/Application.java
@@ -0,0 +1,10 @@
+package info.novatec.micronaut.camunda.bpm.mvnexample;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ maven-example/src/test/java/info/novatec/micronaut/camunda/bpm/mvnexample/MavenExampleTest.java
@@ -0,0 +1,21 @@
+package info.novatec.micronaut.camunda.bpm.mvnexample;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class MavenExampleTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
